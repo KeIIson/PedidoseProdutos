b38503bd5367f58b76a03cbe68419ffcd0755d46
Kellzon <kelsonnb@gmail.com>
2026-08-28 11:03:12 -0300

    up endpoint de produtos e pedidos

--- apps/app.py
+++ apps/app.py
@@ -17,5 +17,29 @@ sqs = boto3.client("sqs", region_name=AWS_REGION)
 PRODUCTS = [
     {"ID":1, "nome": "Noteook", "preco": 3700.00},
     {"ID":2, "nome": "Mouse", "preco": 50.00},
-    {"ID":3, "nome": "Teclado", "preco": 150,00}
+    {"ID":3, "nome": "Teclado", "preco": 150.00}
 ]
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"status": "API rodando na EC2!"}), 200
+
+# Endpoint da API de Produtos
+@app.route("/produtos", methods=["GET"])
+def get_produtos():
+    return jsonify(PRODUCTS), 200
+
+
+# Endpoint da API de Pedidos
+@app.route("/pedidos", methods=["POST"])
+def create_pedido():
+    data = request.get_json()
+
+    if not data or "produto_id" not in data or "quantidade" not in data:
+        return jsonify({"erro": "Dados inválidos. Envie 'produto_id' e 'quantidade'."}), 400
+
+    pedido = {
+        "produto_id": data["produto_id"],
+        "quantidade": data["quantidade"],
+        "status": "CRIADO"
+    }
+    
